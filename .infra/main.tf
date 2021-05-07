a6d094b5e1c10de2b3728cd4461aee02eb760af7
Adam Stoffel <2475873+adamstoffel@users.noreply.github.com>
2021-05-06 17:35:29 -0700

    Terraform site deployment

--- .infra/main.tf
+++ .infra/main.tf
@@ -4,9 +4,9 @@ locals {
 
 terraform {
   backend "azurerm" {
-    container_name       = "tfstate"
-    key                  = "pwdgame-state"
-    # Remaining parameters from -backend-config argument
+    container_name = "tfstate"
+    key            = "pwdgame-state"
+    # Remaining parameters from '-backend-config' arguments
   }
   required_providers {
     azurerm = {
@@ -20,11 +20,10 @@ provider "azurerm" {
   features {}
   environment = var.azure_environment
   # Service principal credential from env variables
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret#configuring-the-service-principal-in-terraform
 }
 
-resource "random_string" "random" {
-  length           = 6
+resource "random_string" "service_suffix" {
+  length           = 8
   special          = true
   upper            = false
   override_special = ""
@@ -34,4 +33,24 @@ resource "random_string" "random" {
 resource "azurerm_resource_group" "main_rg" {
   name     = var.azure_resource_group_name
   location = var.azure_region
+}
+
+resource "azurerm_app_service_plan" "main_plan" {
+  name                = "${local.service_prefix}-apps-${random_string.service_suffix}"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "cat_game" {
+  name                = "${local.service_prefix}-cat_game-${random_string.service_suffix}"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  app_service_plan_id = azurerm_app_service_plan.main_plan.id
+  https_only          = true
 }
