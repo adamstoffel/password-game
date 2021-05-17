371baccd9b09f79afb4dd1f1ad90af1bf7071b56
Adam Stoffel <2475873+adamstoffel@users.noreply.github.com>
2021-05-16 17:12:37 -0700

    Add dog game code

---
+++ dog-game/src/components/AuthenticationComponent.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { User } from "../../../backend-api/models/User";
+import LoginForm from "./LoginForm.vue"
+import RegistrationForm from "./RegistrationForm.vue"
+import PasswordResetForm from "./PasswordResetForm.vue"
+
+@Component({
+  components: {
+    LoginForm,
+    RegistrationForm,
+    PasswordResetForm
+  },
+})
+export default class AuthenticationComponent extends Vue {
+  @Prop(Function)
+  readonly setUserCallback?: (user: User) => void;
+
+  loginType = 0; // 0 = login; 1 = register; 2 = password reset
+
+  goToLogin() {
+    this.loginType = 0;
+  }
+
+  goToRegister() {
+    this.loginType = 1;
+  }
+
+  goToPasswordReset() {
+    this.loginType = 2;
+  }
+}
+</script>
+
+<template>
+  <div class="d-flex flex-column h-100 justify-content-center align-items-stretch">
+    <h2 class="text-center">Dog Game - trust me with your Cat password!</h2>
+    <div class="d-flex justify-content-center align-items-center">
+      <transition name="fade" mode="out-in">
+        <LoginForm v-if="loginType === 0" key="login"
+                   :setUserCallback="setUserCallback" :registerCallback="goToRegister" :passwordResetCallback="goToPasswordReset" />
+      </transition>
+    </div>
+  </div>
+</template>
